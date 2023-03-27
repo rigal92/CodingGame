0f38b4c7f3fd6cb449f6372e86b1bd2e7a3545fc
rigal92 <rigal@live.it>
2023-03-27 18:05:46 +0200

    Switching to new branch

--- Mars_lander/main.py
+++ Mars_lander/main.py
@@ -1,21 +1,40 @@
 import pandas as pd
 import sys
 
-
-
-# surface_n = int(input())  # the number of points used to draw the surface of Mars.
-# surf = pd.DataFrame([[int(j) for j in input().split()] for i in range(surface_n)],columns = ["x","y"])
 surface = [[0,100], [1000,500], [1500,1500], [3000,1000], [4000,150], [5500,150], [6999,800]]
 surf = pd.DataFrame(surface,columns = ["x","y"])
 
 
+# surface_n = int(input())  # the number of points used to draw the surface of Mars.
+# surf = pd.DataFrame([[int(j) for j in input().split()] for i in range(surface_n)],columns = ["x","y"])
 
-print(surf.values,file=sys.stderr, flush = True)
+#find flat land
+i = surf[surf.diff().y == 0].index.values
+flat_m,flat_M = surf.loc[i.min()-1:i.max(), "x"]
+
+
+# print(surf.values,file=sys.stderr, flush = True)
 # game loop
 while True:
 
     x, y, h_speed, v_speed, fuel, rotate, power = [int(i) for i in input().split()]
 
+    if x<flat_m:
+        a = -45
+        p = 4
+
+    elif x>flat_M:
+        a = 45
+        p = 4
+    else:
+        if h_speed != 0:
+            a = int(45 * h_speed/abs(h_speed))
+            p = 1
+        else:
+            a = 0
+            p = 0
 
 
-    print("-20 3")
+
+
+    print(f"{a} {p}")
